fedc29c3c1a3a6a5f8c279f9795825c9e72c8855
pastak <pasta0915@gmail.com>
2020-03-10 16:37:58 +0900

    install git by homebrew

--- initialize.sh
+++ initialize.sh
@@ -4,7 +4,7 @@ DOT_DIRECTORY=$(pwd)
 xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update
-brew install peco mercurial zsh rbenv ruby-build tmux reattach-to-user-namespace hub wget gnutls vim coreutils
+brew install git peco mercurial zsh rbenv ruby-build tmux reattach-to-user-namespace hub wget gnutls vim coreutils
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
 mkdir -p ~/.tmux/plugins ~/bin/
@@ -15,10 +15,8 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git submodule init
 git submodule update
 
-# download diff-hightlight
-wget https://raw.github.com/git/git/master/contrib/diff-highlight/diff-highlight -o diff-highlight
-chmod +x diff-highlight
-mv diff-highlight ~/bin
+# enable diff-hightlight
+sudo ln -s /usr/local/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
 
 # tmux-pbcopy
 curl https://gist.githubusercontent.com/pastak/8223238b0c46fcc82db6/raw/gistfile1.txt > ~/bin/tmux-pbcopy
